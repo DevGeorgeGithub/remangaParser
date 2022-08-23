cae0a132cf8c6fd76f8770e26b24777568968f28
Matsuok <matsuokisho@gmail.com>
2022-08-23 21:06:05 +0300

    ChaptersForTittle

--- main.go
+++ main.go
@@ -6,26 +6,32 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
+
 	"golang.org/x/net/html/charset"
 )
 
 var (
-	body []byte
+	body     []byte
 	textJson map[string]interface{}
-	
-	defaultUrl = "https://api.remanga.org/api/search/catalog/?ordering=-rating&page=1&count=30"
-	dirs string
-	rus_names string
-	chapter string
-	url string
-	description string
-	Images string
-	chaptersUrl string
-	numberChapters string
 
-	api []string
+	defaultUrl     = "https://api.remanga.org/api/search/catalog/?ordering=-rating&page=1&count=30"
+	dirs           string
+	rus_names      string
+	chapter        string
+	url            string
+	description    string
+	Images         string
+	chaptersUrl    string
+	numberChapters string
+	pageNumber     = 1
+	chaptersUrlId  string
+
+	api       []string
 	arrayDirs []string
 )
+
+var stopNumberChapters = !strings.Contains(numberChapters, "Глава 0") 
 
 func main() {
 
@@ -37,17 +43,20 @@ func main() {
 
 func urlGet() {
 
+
 	if url == "" {
 		url = defaultUrl
 	} else if url == defaultUrl {
 		url = api[0]
 	} else if url == api[0] {
 		url = chapter
-	}else if url == chapter {
+	} else if url == chapter {
+		url = chaptersUrl
+	} else if url != chaptersUrl && stopNumberChapters {
 		url = chaptersUrl
 	}
-}
 
+}
 func parser() {
 
 	fmt.Println(url)
@@ -62,17 +71,20 @@ func valuesJson() {
 
 	json.Unmarshal(body, &textJson)
 
-	if url == defaultUrl || url == chaptersUrl{
+	if url == defaultUrl || url == chaptersUrl {
 		for _, item := range textJson["content"].([]interface{}) {
 			dirs += fmt.Sprintf("%v", item.(map[string]interface{})["dir"])
 			api = append(api, "https://api.remanga.org/api/titles/"+dirs)
 			arrayDirs = append(arrayDirs, "https://remanga.org/manga/"+dirs)
-	
+
 			if url == defaultUrl {
 				rus_names += fmt.Sprintf("%v", item.(map[string]interface{})["rus_name"]) + ","
-			}else {
+			} else {
 				numberChapters += "Глава " + fmt.Sprintf("%v", item.(map[string]interface{})["chapter"]) + ","
 			}
+		}
+		if url == chaptersUrl && stopNumberChapters {
+			pageNumber += 1
 		}
 	}
 
@@ -80,25 +92,26 @@ func valuesJson() {
 		mapJson := textJson["content"].(map[string]interface{})
 		for key, value := range mapJson {
 			switch key {
-				case "description":
-					description += fmt.Sprintf("%v", value)
-				case "branches":
-					for _, item := range value.([]interface{}) {
-						chaptersUrl = "https://api.remanga.org/api/titles/chapters/?branch_id=" + fmt.Sprintf("%v", item.(map[string]interface{})["id"]) + "&count=60&ordering=-index&page=1&user_data=1"
+			case "description":
+				description += fmt.Sprintf("%v", value)
+			case "branches":
+				for _, item := range value.([]interface{}) {
+					chaptersUrlId = fmt.Sprintf("%v", item.(map[string]interface{})["id"])
+				}
+			case "first_chapter":
+				for key, value2 := range value.(map[string]interface{}) {
+					if key == "id" {
+						chapter = "https://api.remanga.org/api/titles/chapters/" + fmt.Sprintf("%v", value2)
 					}
-				case "first_chapter":
-					for key, value2 := range value.(map[string]interface{}) {
-						if key == "id" {
-							chapter = "https://api.remanga.org/api/titles/chapters/" + fmt.Sprintf("%v", value2)
-						}
-					}
-				case "pages":
-					for _, item := range value.([]interface{}) {
-						Images += fmt.Sprintf("%v", item.(map[string]interface{})["link"]) + ", "
-					}
+				}
+			case "pages":
+				for _, item := range value.([]interface{}) {
+					Images += fmt.Sprintf("%v", item.(map[string]interface{})["link"]) + ", "
+				}
 			}
 		}
 	}
+	chaptersUrl = "https://api.remanga.org/api/titles/chapters/?branch_id=" + chaptersUrlId + "&count=60&ordering=-index&page=" + fmt.Sprint(pageNumber) + "&user_data=1"
 }
 
 func createJson() {
@@ -113,9 +126,7 @@ func createJson() {
 	f.WriteString("\"Chapters\":" + "\"" + numberChapters + "\"" + ",")
 	f.WriteString("\"Images\":" + "\"" + Images + "\"}")
 	f.Close()
-
-	if url != chaptersUrl  {
+	if  !strings.Contains(numberChapters, "Глава 0") {
 		defer main()
 	}
 }
-
