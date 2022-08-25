6626df7b4b867dd376b4423dbab982b545089bb3
Matsuok <matsuokisho@gmail.com>
2022-08-25 09:37:11 +0300

    needMoreTests

--- main.go
+++ main.go
@@ -8,20 +8,22 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 
 	"golang.org/x/net/html/charset"
 )
 
 var (
-	body     []byte
-	textJson map[string]interface{}                                                                  
+	body                                                                       []byte
+	textJson                                                                   map[string]interface{}
 	rus_names, url, description, Images, urlChapters, numberChapters, mainPage string
-	numberPageChapters                                                        float64 = 1
-	lastPageNumber                                                            float64
-	counterDescription, countPagesImages, countRepeatGetImages, countBrenchId int
-	arrayIdChapters, arrayApiTitles, arrayBrenchId                            []string
-	numberPage                                                                = 1
-	totalPages                                                                = 2
+	numberPageChapters                                                         float64 = 1
+	lastPageNumber                                                             float64
+	counterDescription, countPagesImages, countRepeatGetImages, countBrenchId  int
+	arrayIdChapters, arrayApiTitles, arrayBrenchId                             []string
+	numberPage                                                                 = 1
+	totalPages                                                                 = 2
+	wg                                                                         = sync.WaitGroup{}
 )
 
 func main() {
@@ -30,9 +32,9 @@ func main() {
 	}
 }
 
+// получить Description chapters img 1 манги 2 страницы последней главы   => готово
 func urlGet() {
-	// wg := sync.WaitGroup{}
-  // wg.Add(3)
+	fmt.Println(url)
 	switch url {
 	case "":
 		getMainPage()
@@ -47,7 +49,8 @@ func urlGet() {
 
 func getMainPage() {
 	if numberPage != totalPages {
-		url = "https://api.remanga.org/api/search/catalog/?ordering=-rating&page=" + fmt.Sprintf("%v", numberPage) + "&count=30"
+		mainPage = "https://api.remanga.org/api/search/catalog/?ordering=-rating&page=" + fmt.Sprintf("%v", numberPage) + "&count=30"
+		url = mainPage
 	}
 	numberPage++
 	parser()
@@ -63,16 +66,21 @@ func getMainPage() {
 			totalPages = i
 		}
 		createJson()
+		wg.Add(3)
 	}
 }
 func changeUrlApiTitles() {
 	url = arrayApiTitles[counterDescription]
 	parser()
 	Backend()
-	createJson()
+	go func() {
+		createJson()
+	}()
 	if counterDescription != 29 {
 		counterDescription++
-		defer changeUrlApiTitles()
+		changeUrlApiTitles()
+	} else {
+		wg.Done()
 	}
 }
 
@@ -80,60 +88,85 @@ func changeUrlChapters() {
 	urlChapters = "https://api.remanga.org/api/titles/chapters/?branch_id=" + arrayBrenchId[countBrenchId] + "&count=60&ordering=-index&page=" + fmt.Sprint(numberPageChapters) + "&user_data=1"
 	url = urlChapters
 	parser()
-	doUrlChapter()
-	changePageChapters()
-	createJson()
+
+	go func() {
+		changePageChapters()
+		createJson()
+	}()
 
 	if numberPageChapters != lastPageNumber {
-		defer changeUrlChapters()
+		doUrlChapter()
+		changeUrlChapters()
 	} else {
 		if countBrenchId != 31 {
 			countBrenchId++
 			numberPageChapters = 0
 			changeUrlChapters()
+		} else {
+			countBrenchId = 0
+			wg.Done()
 		}
 	}
 }
 
 func getImages() {
-	url = arrayIdChapters[countRepeatGetImages]
+	// fmt.Println(len(arrayIdChapters))
+	url = arrayIdChapters[countRepeatGetImages] //index out of range [2915] with length 2915
 	parser()
-
 	for key, value := range textJson["content"].(map[string]interface{}) {
 		switch key {
 		case "pages":
 			for key, value := range value.([]interface{}) {
 				switch key {
-				case countPagesImages:
-					countPagesImages++
-					if fmt.Sprintf("%T", value) == "[]interface {}" {
-						for _, value := range value.([]interface{}) {
-							Images += fmt.Sprintf("%v", value.(map[string]interface{})["link"]) + ","
-						}
-					} else {
-						for key, value := range value.(map[string]interface{}) {
-							switch key {
-							case "link":
-								Images += fmt.Sprintf("%v", value) + ","
+					case countPagesImages:
+						countPagesImages++
+						if fmt.Sprintf("%T", value) == "[]interface {}" {
+							for _, value := range value.([]interface{}) {
+								Images += fmt.Sprintf("%v", value.(map[string]interface{})["link"]) + ","
+							}
+							} else {
+								for key, value := range value.(map[string]interface{}) {
+								switch key {
+								case "link":
+									Images += fmt.Sprintf("%v", value) + ","
+								}
 							}
 						}
 					}
 				}
 			}
 		}
-	}
-	createJson()
+		go func() {
+		createJson()
+	}()
 	countPagesImages = 0
 	if countRepeatGetImages < len(arrayIdChapters) {
 		countRepeatGetImages++
-		defer getImages()
+		getImages()
+	} else {
+			countRepeatGetImages = 0
+			counterDescription = 0
+			url = ""
+			arrayApiTitles = nil
+			arrayIdChapters = nil
+			arrayBrenchId = nil
+			defer main()
+			wg.Done()
+			wg.Wait()
+		}
 	}
-	if countRepeatGetImages == len(arrayIdChapters) - 1 {
-		countRepeatGetImages = 0
-				url = ""
-				defer main()
-	}
-}
+	// if countRepeatGetImages == len(arrayIdChapters)-1 {
+	// 	countRepeatGetImages = 0
+	// 	counterDescription = 0
+	// 	url = ""
+	// 	wg.Done()
+	// 	wg.Wait()
+	// 	arrayApiTitles = nil
+	// 	arrayIdChapters = nil
+	// 	arrayBrenchId = nil
+	// 	defer main()
+	// }
+// }
 
 func parser() {
 	resp, _ := http.Get(url)
